fdd7a1eb23d871f035b6c790a56075530ec4ae43
williamsca <williams.colinandrew@gmail.com>
2023-01-28 13:21:47 -0500

    Q3

--- homeworks/HW1/20230128 ProdFunc Est CW.R
+++ homeworks/HW1/20230128 ProdFunc Est CW.R
@@ -1,3 +1,7 @@
+# Replication files for Griliches and Mairesse (1995)
+# Author: Colin Williams
+# Last updated: 28 Jan 2023
+
 rm(list = ls())
 
 dir <- dirname(dirname(rstudioapi::getSourceEditorContext()$path))
@@ -26,27 +30,46 @@ table(dt$sic3)
 # Q2: Total, between, within, and random effects estimators ----
 fmla <- "ldsal ~ -1 + lemp + ldnpt + ldrst + as.factor(yr):d357" # as.factor(yr) + as.factor(index)
 
-# Column (3) of Table 3
-plm.ols <- plm(fmla, data = dt, model = c("pooling"), index = c("yr", "index"))
-plm.with <- plm(fmla, data = dt, model = c("within"), index = c("yr", "index"))
-plm.bet <- plm(fmla, data = dt, model = c("between"), index = c("yr", "index"))
-# plm.re <- plm(fmla, data = dt, model = c("random"), index = c("yr", "index"))
+# * Full sample ----
+plm.ols <- plm(fmla, data = dt2, model = "pooling", index = c("index", "yr")) # column (3) of Table 3
+plm.with <- plm(fmla, data = dt2, model = "within", index = c("index", "yr"), effect = "twoway")
+plm.bet <- plm(fmla, data = dt2, model = "between", index = c("index", "yr"))
+plm.re <- plm(fmla, data = dt, model = "random", index = c("index", "yr"))
 
-stargazer(lm.ols, plm.with, plm.bet, type = "text", omit = c("yr", "d357", "index"),
+stargazer(plm.ols, plm.with, plm.bet, plm.re, type = "text", omit = c("yr", "d357", "index"),
           covariate.labels = c("Labor", "Physical Capital", "R&D Capital"),
-          column.labels = c("Total", "Within",))
+          dep.var.labels = "Log sales",
+          column.labels = c("Total OLS", "Within","Between", "Random"),
+          title = "Production Function Parameters: Full Sample")
 
-# These both yield column (2) of Table 3
-lm.ols_bal <- lm(fmla, data = dt[yrsPresent == 4])
-plm.with <- plm(fmla, data = dt[yrsPresent == 4], model = c("within"), index = c("yr", "index"))
-stargazer(lm.ols_bal, plm.with, type = "text", omit = c("yr", "d357", "index"), 
-          covariate.labels = c("Labor", "Physical Capital", "R&D Capital"))
+# * Balanced panel ----
+plm.olsB <- plm(fmla, data = dt[yrsPresent == 4], model = "pooling", index = c("index", "yr")) # column (1) of Table 3
+plm.withB <- plm(fmla, data = dt[yrsPresent == 4], model = "within", index = c("index", "yr"), effect = "twoway") # column (2) of Table 3
+plm.betB <- plm(fmla, data = dt[yrsPresent == 4], model = "between", index = c("index", "yr"))
+plm.reB <- plm(fmla, data = dt[yrsPresent == 4], model = "within", index = c("index", "yr"))
+
+stargazer(plm.olsB, plm.withB, plm.betB, plm.reB, type = "text", omit = c("yr", "d357", "index"), 
+          covariate.labels = c("Labor", "Physical Capital", "R&D Capital"), 
+          dep.var.labels = "Log sales",
+          column.labels = c("Total OLS", "Within","Between", "Random"),
+          title = "Production Function Parameters: Balanced Panel")
           
+# Q3: Hausman test of RE vs FE ----
+
+# full sample
+phtest(plm.with, plm.re) # p-value is significant at 1% --> reject RE model
+
+# balanced panel
+phtest(plm.withB, plm.reB) # p-value is insignificant at 5% --> fail to reject RE model
 
 
-plm.bet_bal <- plm(fmla, data = dt[yrsPresent == 4], model = c("within"), index = c("yr", "index"))
-plm.ht_bal <- plm(fmla, data = dt[yrsPresent == 4], model = c("within"), index = c("yr", "index"))
-plm.bet_bal <- plm(fmla, data = dt[yrsPresent == 4], model = c("within"), index = c("yr", "index"))
+# Q4: Olley-Pakes estimator ----
+# i) Predict sales with labor and a polynomial in the predetermined capital stock (ldnpt and ldrst) and investment (ldinv)
+fmla.op1 <- "ldsal ~ -1 + lemp + as.factor(yr):d357 + poly(ldnpt, 2) + poly(ldrst, 2) + poly(ldinv, 2) + as.factor(yr)"
+lm.op <- lm(fmla.op1, data = dt)
+stargazer(lm.op, type = "text", omit = c("d357", "poly"))
 
-stargazer(plm.bet, plm.bet_bal, type = "text", omit = c("yr", "d357", "index"), 
-          covariate.labels = c("Labor", "Physical Capital", "R&D Capital"))
+# ii) 
+
+
+
