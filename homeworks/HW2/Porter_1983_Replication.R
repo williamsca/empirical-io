9f8cea89a8e7ff794a21ca5d159df0a68cc4f1fa
williamsca <williams.colinandrew@gmail.com>
2023-02-07 19:44:26 -0500

    missing data for Q2

--- homeworks/HW2/Porter_1983_Replication.R
+++ homeworks/HW2/Porter_1983_Replication.R
@@ -10,6 +10,19 @@ setwd(dir)
 pacman::p_load(data.table, haven, stargazer, plm)
 # haven: read .dta files
 # stargazer: create summary tables
-# plm: panel estimation techniques
 
 dt <- as.data.table(read_dta("HW2/railway.dta"))
+setcolorder(dt, neworder = c("week", "month", "gr", "tgq", "lakes", "po"))
+
+# Table 2: Summary Statistics ----
+
+stargazer(dt, summary  = TRUE, nobs = FALSE, type = "text", keep = c("gr", "tgq", "lakes", "po"))
+
+
+# Table 3: Estimation Results ----
+
+dt[, month4 := ((floor((week - 1) / 4))) %% 13 + 1]
+
+# TODO: define DM1, ..., DM4
+
+# TODO: ask Federico about Q_t, p_t?
