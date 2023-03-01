a369b7fef0fd658b10e975a3c84f88a9feb41cc2
williamsca <williams.colinandrew@gmail.com>
2023-03-01 09:48:33 -0500

    HW3 up to merger simulation

--- homeworks/HW3/Logit.R
+++ homeworks/HW3/Logit.R
@@ -7,21 +7,25 @@ rm(list = ls())
 dir <- dirname(dirname(rstudioapi::getSourceEditorContext()$path))
 setwd(dir)
 
-pacman::p_load(data.table, haven, stargazer, ggplot2, fixest)
+pacman::p_load(data.table, haven, stargazer, ggplot2, antitrust)
+# antitrust: nested logit demand estimation and merger simulation
 
 # Question 1: Download airline dataset ----
 dt <- as.data.table(read_dta("HW3/airline1997data.dta"))
-
-# NOTE: unique by origin, dest, tkcarrier, T100nonstop
 
 dt[tkcarrier %in% c("AA", "CO", "DL", "NW", "TW", "UA", "US"), carrierType := "Legacy"]
 dt[tkcarrier == "WN", carrierType :=  "Southwest"] 
 dt[is.na(carrierType), carrierType := "Other LCC"]
 
-# * Compute how many markets are served by an origin-carrier pair ----
+# Compute how many markets are served by an origin-carrier pair
 dt[, nDest := uniqueN(dest), by = .(origin, tkcarrier)]
-dt[, mktShare := totalpassengers / marketsize]
-dt[, outsideShare := 1 - sum(mktShare), by = .(market)]
+dt[, rivalNDest := sum(nDest) - nDest, by = .(market)] # for IV - see BLP 1995, eqn (5.8)
+
+# Compute market shares
+dt[, prodMktShare := totalpassengers / marketsize] # product market share
+dt[, outsideShare := 1 - sum(prodMktShare), by = .(market)] # share of the outside option
+dt[, firmMktShare := sum(prodMktShare), by = .(market, tkcarrier)] # account for multi-product firms
+dt[, groupShare := totalpassengers / sum(totalpassengers), by  = .(market)] # within-group share
 
 
 # Question 3: Summary statistics for prices and quantities ----
@@ -58,17 +62,33 @@ stargazer(dt[carrierType == "Other LCC"], summary = TRUE, keep =  c("MdW_oneway_
           type = "text", summary.stat = c("n", "mean", "median", "sd", "min", "max"))
 
 # Simple OLS ----
-fmla <- "log(mktShare) ~ -1 + MdW_oneway_itinfare_ticket + marketdistanceticket + nDest + log(outsideShare)"
+fmla <- "log(prodMktShare) ~ -1 + MdW_oneway_itinfare_ticket + marketdistanceticket + nDest + log(outsideShare)"
 utility.ols <- lm(fmla, data = dt)
 stargazer(utility.ols, type = "text")
 
+# * Residual plot
 ggplot(data = as.data.table(utility.ols$residuals), mapping = aes(x = V1)) +
   geom_histogram(bins = 90) +
   labs(x = "Residuals", y = "Frequency")
 
-# TODO: compute the elasticity of demand for each good in each market?
+alpha.ols <- coefficients(utility.ols)["MdW_oneway_itinfare_ticket"]
 
-# MC = P(1 + 1/e), but what about multi-product firms?
+# * Demand elasticities and markups ----
+dt[, e.ols := -alpha.ols*MdW_oneway_itinfare_ticket*(1-firmMktShare)]
+dt[, mc.ols := MdW_oneway_itinfare_ticket + 1 / (alpha.ols*(1-firmMktShare))]
+dt[, markup.ols := (MdW_oneway_itinfare_ticket - mc.ols)/MdW_oneway_itinfare_ticket]
+
+ggplot(data = dt, mapping = aes(x = e.ols)) +
+  geom_histogram(bins = 50) +
+  scale_x_continuous() +
+  labs(x = "Demand Elasticity", y = "Frequency") +
+  theme_light()
+
+ggplot(data = dt, mapping = aes(x = markup.ols)) +
+  geom_histogram(bins = 50) +
+  scale_x_continuous() +
+  labs(x = "Lerner Index", y = "Frequency") +
+  theme_light()
 
 # OLS w/ FEs ----
 fmla.fe <- paste0(fmla, " + tkcarrier")
@@ -76,27 +96,94 @@ utility.olsfe <- lm(fmla.fe, data = dt)
 stargazer(utility.ols, utility.olsfe, type = "text", omit = c("tkcarrier"),
           add.lines = list(c("Airline FEs", "No", "Yes")),
           omit.stat = c("F"))
+alpha.fe <- coefficients(utility.olsfe)["MdW_oneway_itinfare_ticket"]
 
-# TODO: compute the elasticity of demand for each good in each market?
-
+# * Residual plot
 ggplot(data = as.data.table(utility.olsfe$residuals), mapping = aes(x = V1)) +
   geom_histogram(bins = 90) +
   labs(x = "Residuals", y = "Frequency")
 
-# Compute the instruments (BLP 1995, eqn (5.8)) ----
-dt[, rivalNDest := sum(nDest) - nDest, by = .(market)]
+# * Demand elasticities and markups ----
+dt[, e.fe := -alpha.fe*MdW_oneway_itinfare_ticket*(1-firmMktShare)]
+dt[, mc.fe := MdW_oneway_itinfare_ticket + 1 / (alpha.fe*(1-firmMktShare))]
+dt[, markup.fe := (MdW_oneway_itinfare_ticket - mc.fe)/MdW_oneway_itinfare_ticket]
 
+ggplot(data = dt, mapping = aes(x = e.fe)) +
+  geom_histogram(bins = 50) +
+  scale_x_continuous() +
+  labs(x = "Demand Elasticity", y = "Frequency", title = "OLS with Fixed Effects") +
+  theme_light()
+
+ggplot(data = dt, mapping = aes(x = markup.fe)) +
+  geom_histogram(bins = 50) +
+  scale_x_continuous(limits = c(0, 11)) +
+  labs(x = "Lerner Index", y = "Frequency", title = "OLS with Fixed Effects") +
+  theme_light()
+
+# IV without FEs ----
 fmla.fs <- "MdW_oneway_itinfare_ticket ~ -1 + nDest + rivalNDest"
 iv.fs <- lm(fmla.fs, data = dt)
 dt$p_hat <- predict(iv.fs, dt)
 
-fmla.ss <- "log(mktShare) ~ -1 + p_hat + marketdistanceticket + log(outsideShare)"
+fmla.ss <- "log(prodMktShare) ~ -1 + p_hat + marketdistanceticket + log(outsideShare)"
 iv.ss <- lm(fmla.ss, data = dt)
 stargazer(utility.ols, iv.ss, type = "text")
+alpha.iv <- coefficients(iv.ss)["p_hat"]
 
+# * Residual plot
 ggplot(data = as.data.table(iv.ss$residuals), mapping = aes(x = V1)) +
   geom_histogram(bins = 90) +
   labs(x = "Residuals", y = "Frequency")
 
+# * Demand elasticities and markups ----
+dt[, e.iv := -alpha.iv*MdW_oneway_itinfare_ticket*(1-firmMktShare)]
+dt[, mc.iv := MdW_oneway_itinfare_ticket + 1 / (alpha.iv*(1-firmMktShare))]
+dt[, markup.iv := (MdW_oneway_itinfare_ticket - mc.iv)/MdW_oneway_itinfare_ticket]
+
+ggplot(data = dt, mapping = aes(x = e.iv)) +
+  geom_histogram(bins = 50) +
+  scale_x_continuous() +
+  labs(x = "Demand Elasticity", y = "Frequency", title = "IV") +
+  theme_light()
+
+ggplot(data = dt, mapping = aes(x = markup.iv)) +
+  geom_histogram(bins = 50) +
+  scale_x_continuous(limits = c(0, .75)) +
+  labs(x = "Lerner Index", y = "Frequency", title = "IV") +
+  theme_light()
 
 # Nested Logit ----
+fmla.nl <- "log(prodMktShare) ~ -1 + p_hat + marketdistanceticket + log(outsideShare) + log(groupShare)"
+iv.nl <- lm(fmla.nl, data = dt)
+stargazer(utility.ols, iv.nl, type = "text")
+alpha.nl <- coefficients(iv.nl)["p_hat"]
+
+# * Residual plot
+ggplot(data = as.data.table(iv.nl$residuals), mapping = aes(x = V1)) +
+  geom_histogram(bins = 90) +
+  labs(x = "Residuals", y = "Frequency")
+
+# * Demand elasticities and markups ----
+dt[, e.nl := -alpha.nl*MdW_oneway_itinfare_ticket*(1-firmMktShare)]
+dt[, mc.nl := MdW_oneway_itinfare_ticket + 1 / (alpha.nl*(1-firmMktShare))]
+dt[, markup.nl := (MdW_oneway_itinfare_ticket - mc.nl)/MdW_oneway_itinfare_ticket]
+
+ggplot(data = dt, mapping = aes(x = e.nl)) +
+  geom_histogram(bins = 50) +
+  scale_x_continuous() +
+  labs(x = "Demand Elasticity", y = "Frequency", title = "Nested Logit") +
+  theme_light()
+
+ggplot(data = dt, mapping = aes(x = markup.iv)) +
+  geom_histogram(bins = 50) +
+  scale_x_continuous(limits = c(0, .75)) +
+  labs(x = "Lerner Index", y = "Frequency", title = "Nested Logit") +
+  theme_light()
+
+# Multi-nested Logit & Merger Simulations ----
+v.fields <- c("market", "origin", "dest", "tkcarrier", "T100nonstop", "marketdistanceticket", 
+              "totalpassengers", "MdW_oneway_itinfare_ticket", "marketsize", "carrierType", 
+              "nDest")
+write_dta(dt[, ..v.fields], "HW3/airline1997dataMERGERSIM.dta")
+
+
